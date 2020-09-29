c8736187bf2f03df3857f365887cbda7888be916
RicXD <291261411@qq.com>
2020-09-29 12:04:05 +1000

    Update create_input_files.py: the default path is Google Drive path

--- create_input_files.py
+++ create_input_files.py
@@ -1,11 +1,13 @@
 from utils import create_input_files
 
+base_path = 'drive/My Drive/NLP/a-PyTorch-Tutorial-to-Image-Captioning'
+
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+                       karpathy_json_path=base_path + '/dataset_coco.json',
+                       image_folder=base_path,
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder=base_path + '/input_files',
                        max_len=50)
